64f069a2051c8732e65c9fb87f1a8ff0869f1d79
geueyrgrrhstdgfehdjd-wq <geueyrgrrhstdgfehdjd@gmail.com>
2026-08-26 05:14:03 +0700

    Create app.js

---
+++ public/js/app.js
@@ -0,0 +1,2 @@
+function flashSuccess(msg){Swal.fire({icon:'success',title:msg,toast:true,position:'top-end',timer:2500,showConfirmButton:false,background:'#14141f',color:'#e5e7eb'});}
+function flashError(msg){Swal.fire({icon:'error',title:msg,toast:true,position:'top-end',timer:3500,showConfirmButton:false,background:'#14141f',color:'#e5e7eb'});}
